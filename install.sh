538e7e6a6da86ff52f36f5c180770f7deb3d1aaa
rml1997 <rml1997@users.noreply.github.com>
2016-12-03 11:03:01 +0000

    Add firebird, remove redis

--- install.sh
+++ install.sh
@@ -16,16 +16,12 @@ sudo /usr/bin/composer self-update
 
 # Add PHP7.0 Repository
 LC_ALL=C.UTF-8 sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-add-repository ppa:rwky/redis -y
 sudo apt-get update
-
-# Update Heroku Toolbelt
-wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # Install PHP7.0 & Redis
 sudo apt-get install -qq php7.0-fpm php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-mysql php7.0-phpdbg \
 php7.0-mbstring php7.0-gd php7.0-imap php7.0-ldap php7.0-pgsql php7.0-pspell php7.0-recode php7.0-tidy php7.0-dev \
-php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml redis-server
+php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml firebird2.5-classic
 sudo apt-get purge -qq apache2 mysql-server mysql-client
 
 # Stop all the services
@@ -51,20 +47,6 @@ sudo ln -s /etc/nginx/sites-available/c9 /etc/nginx/sites-enabled/c9
 sudo sed -i 's/user = www-data/user = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
 sudo sed -i 's/group = www-data/group = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
 sudo sed -i 's/pm = dynamic/pm = ondemand/g' /etc/php/7.0/fpm/pool.d/www.conf # Reduce number of processes..
-
-# Install Redis Driver
-cd ~
-git clone https://github.com/phpredis/phpredis.git
-cd phpredis
-git checkout php7
-phpize
-./configure
-make && sudo make install
-cd ..
-rm -rf phpredis
-sudo sh -c 'echo "extension=redis.so" > /etc/php/7.0/mods-available/redis.ini'
-sudo ln -sf /etc/php/7.0/mods-available/redis.ini /etc/php/7.0/fpm/conf.d/20-redis.ini
-sudo ln -sf /etc/php/7.0/mods-available/redis.ini /etc/php/7.0/cli/conf.d/20-redis.ini
 
 # Install helper
 sudo wget https://raw.githubusercontent.com/GabrielGil/c9-lemp/master/lemp --output-document=/usr/bin/lemp
